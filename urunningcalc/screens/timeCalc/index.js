bcd820da44c558ae306681f38e9cb169cfd3ed6e
Sergei Zaitsev <zaitsev.sergey.93@gmail.com>
2024-02-28 22:53:57 +0300

    feat: formik added to add new fields

--- urunningcalc/screens/timeCalc/index.js
+++ urunningcalc/screens/timeCalc/index.js
@@ -1,13 +1,38 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import TimeInput from "../../components/timeInput";
+import { FieldArray, Form, Formik } from "formik";
+import { Button } from "react-native-paper";
 
 const TimeCalc = () => {
     return (
-        <View>
-            <TimeInput />
-        </View>
+        <Formik initialValues={initialValues}>
+            {({ handleChange, values }) => (
+                <View>
+                    <FieldArray name="splits">
+                        {({ push }) => (
+                            <View>
+                                {values.splits.map((item, key) => (
+                                    <TimeInput
+                                        name={`splits[${key}]`}
+                                        time={item}
+                                        onChange={(value) => handleChange(`splits[${key}]`, value)}
+                                    />
+                                ))}
+                                <Button onPress={() => push(new Date())}>Add</Button>
+                            </View>
+                        )}
+                    </FieldArray>
+
+                    <Text>result</Text>
+                </View>
+            )}
+        </Formik>
     );
+};
+
+const initialValues = {
+    splits: [new Date()],
 };
 
 export default TimeCalc;
