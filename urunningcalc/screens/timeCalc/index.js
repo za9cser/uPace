e2cff3145fe4375e0727f46ff2b4073820c3125d
Sergei Zaitsev <zaitsev.sergey.93@gmail.com>
2024-02-28 21:44:27 +0300

    feat: empty TimeCalc view

---
+++ urunningcalc/screens/timeCalc/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const TimeCalc = () => {
+    return (
+        <View>
+            <Text>TimeCalc</Text>
+        </View>
+    );
+};
+
+export default TimeCalc;
+
+const styles = StyleSheet.create({});
