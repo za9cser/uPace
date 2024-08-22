b4cee426b0026e8d668d27d25322212b808a4c41
Sergei Zaitsev <zaitsev.sergey.93@gmail.com>
2024-08-22 14:33:16 +0300

    feat: calc implemented

--- upace/screens/paceCalc/index.js
+++ upace/screens/paceCalc/index.js
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView, StyleSheet } from "react-native";
+import { Text, ScrollView, StyleSheet } from "react-native";
 import React from "react";
-import { Button, TextInput, useTheme } from "react-native-paper";
+import { TextInput, useTheme } from "react-native-paper";
 import { Formik } from "formik";
 import TimeInput from "../../components/timeInput";
 import { initialValues, paceMode, timeMode } from "./paceCalcUtils";
 import PaceCalcInput from "./paceCalcInput";
+import { TimeSpan } from "timespan";
 
 export default function PaceCalc() {
     const { colors } = useTheme();
@@ -16,22 +17,57 @@ export default function PaceCalc() {
                     <Text style={styles.description} variant="titleMedium">
                         Enter two values to calculate the third
                     </Text>
-                    <PaceCalcInput buttonCaption={"Time"} onCalc={() => {}}>
+                    <PaceCalcInput
+                        buttonCaption={"Time"}
+                        onCalc={() => {
+                            if (values.pace.totalSeconds() === 0 || values.distance === 0 || values.distance === "")
+                                return;
+
+                            const timeSeconds = values.pace.totalSeconds() * values.distance;
+                            const time = new TimeSpan(0, timeSeconds);
+                            setFieldValue("time", time);
+                        }}
+                    >
                         <TimeInput
                             mode={timeMode}
                             time={values.time}
                             onChange={(value) => setFieldValue("time", value)}
                         />
                     </PaceCalcInput>
-                    <PaceCalcInput buttonCaption={"Pace"} onCalc={() => {}}>
+                    <PaceCalcInput
+                        buttonCaption={"Pace"}
+                        onCalc={() => {
+                            if (values.time.totalSeconds() === 0 || values.distance === 0 || values.distance === "")
+                                return;
+
+                            const paceSeconds = values.time.totalSeconds() / values.distance;
+                            const pace = new TimeSpan(0, paceSeconds);
+                            setFieldValue("pace", pace);
+                        }}
+                    >
                         <TimeInput
                             mode={paceMode}
                             time={values.pace}
                             onChange={(value) => setFieldValue("pace", value)}
                         />
                     </PaceCalcInput>
-                    <PaceCalcInput buttonCaption={"Distance"} onCalc={() => {}}>
-                        <TextInput style={{ width: 150 }} />
+                    <PaceCalcInput
+                        buttonCaption={"Distance"}
+                        onCalc={() => {
+                            if (values.time.totalSeconds() === 0 || values.pace.totalSeconds() === 0) return;
+
+                            const distance = values.time.totalSeconds() / values.pace.totalSeconds();
+                            console.log("distance", distance);
+                            setFieldValue("distance", distance.toFixed(2));
+                        }}
+                    >
+                        <TextInput
+                            value={values.distance?.toString() ?? ""}
+                            placeholder="km"
+                            onChangeText={(value) => setFieldValue("distance", value === "" ? 0 : parseFloat(value))}
+                            keyboardType="decimal-pad"
+                            style={{ width: 150, textAlign: "center", height: 40, fontSize: 18 }}
+                        />
                     </PaceCalcInput>
                 </ScrollView>
             )}
