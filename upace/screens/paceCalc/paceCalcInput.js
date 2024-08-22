a6741778c6b3095cb99ad70c67cba1ebc4632ba3
Sergei Zaitsev <zaitsev.sergey.93@gmail.com>
2024-08-22 15:19:58 +0300

    refactor(PaceCalcInput): styles via stylesheet

--- upace/screens/paceCalc/paceCalcInput.js
+++ upace/screens/paceCalc/paceCalcInput.js
@@ -1,17 +1,17 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import React from "react";
 import { Button, useTheme } from "react-native-paper";
 
 const PaceCalcInput = ({ buttonCaption, onCalc, children }) => {
     const { colors } = useTheme();
     return (
-        <View style={{ flexDirection: "row", justifyContent: "flex-end" }}>
-            <View style={{ flex: 3, flexDirection: "row", justifyContent: "flex-end" }}>{children}</View>
-            <View style={{ flex: 1, flexDirection: "row" }}>
+        <View style={styles.container}>
+            <View style={styles.input}>{children}</View>
+            <View style={styles.buttonContainer}>
                 <Button
                     textColor={colors.primary}
-                    style={styles.addButton}
-                    labelStyle={styles.addButtonText}
+                    style={styles.button}
+                    labelStyle={styles.buttonText}
                     onPress={onCalc}
                 >
                     {buttonCaption}
@@ -25,27 +25,22 @@ export default PaceCalcInput;
 
 const styles = StyleSheet.create({
     container: {
-        paddingHorizontal: 16,
-        paddingTop: 8,
+        flexDirection: "row",
+        justifyContent: "flex-end",
     },
-    description: {
-        textAlign: "center",
+    input: {
+        flex: 3,
+        flexDirection: "row",
+        justifyContent: "flex-end",
     },
-    timeInput: {
-        justifyContent: "center",
-        marginTop: 8,
+    buttonContainer: {
+        flex: 1,
+        flexDirection: "row",
     },
-    addButton: {
+    button: {
         paddingVertical: 8,
     },
-    addButtonText: {
+    buttonText: {
         fontSize: 16,
-    },
-    summary: {
-        flexDirection: "row",
-        justifyContent: "space-around",
-        paddingBottom: 25,
-        borderTopColor: "#ccc",
-        borderTopWidth: 1,
     },
 });
