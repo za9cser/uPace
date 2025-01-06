e790001c390c1ddc04d31552ad050db4517b11fe
Sergei Zaitsev <zaitsev.sergey.93@gmail.com>
2025-01-06 22:55:01 +0300

    feat: App and form use TS

---
+++ upace/App.tsx
@@ -0,0 +1,59 @@
+import {
+  PaperProvider,
+  MD3LightTheme,
+  adaptNavigationTheme,
+} from "react-native-paper";
+import TimeCalc from "./screens/timeCalc";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import PaceCalc from "./screens/paceCalc";
+
+const { LightTheme } = adaptNavigationTheme({
+  reactNavigationLight: DefaultTheme,
+});
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <PaperProvider theme={MD3LightTheme}>
+      <NavigationContainer theme={LightTheme}>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              switch (route.name) {
+                case "TimeCalc":
+                  return (
+                    <MaterialIcons name="more-time" size={size} color={color} />
+                  );
+                case "PaceCalc":
+                  return (
+                    <MaterialCommunityIcons
+                      name="run-fast"
+                      size={size}
+                      color={color}
+                    />
+                  );
+                default:
+                  return null;
+              }
+            },
+          })}
+          initialRouteName="Time Calc"
+        >
+          <Tab.Screen
+            name="TimeCalc"
+            component={TimeCalc}
+            options={{ title: "Time Calc" }}
+          />
+          <Tab.Screen
+            name="PaceCalc"
+            component={PaceCalc}
+            options={{ title: "Pace Calc" }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
--- upace/utils/form.ts
+++ upace/utils/form.ts
@@ -1,4 +1,6 @@
-export const focusRef = (ref) => {
+import React from "react";
+
+export const focusRef = (ref: React.MutableRefObject<any>) => {
     const current = ref?.current;
     // if we don't call focus() inside setTimeout and before blur, than keyboard will not show up. But Input will be focused ;-)
     // https://github.com/software-mansion/react-native-screens/issues/472#issuecomment-1239494850
